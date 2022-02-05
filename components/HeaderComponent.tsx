591d6df2686ecfb76f1d2aabb9cb0fc3325f3da2
Mo Almass <mo@horizonbirds.com>
2022-02-05 16:25:53 +0000

    Header UI - Settings dropdown

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { View, Image, Text, StyleSheet, TextInput, FlatList } from "react-native";
 import { Ionicons, Entypo } from '@expo/vector-icons'; 
 
-
-
 export default function HeaderComponent({ navigation }: any) {
 
     const [showSearch, setShowSearch] = useState(false);
@@ -16,17 +14,18 @@ export default function HeaderComponent({ navigation }: any) {
             <View style={styles.logoContainer}>
                 {
                     showSearch ? 
-                    <Entypo style={styles.settingsIcon} name="cross" size={24} color="white" onPress={() => setShowSearch(!showSearch)} /> : 
+                    <Entypo style={styles.searchIcon} name="cross" size={24} color="white" onPress={() => setShowSearch(!showSearch)} /> : 
                     <Ionicons style={styles.searchIcon} name="ios-search" size={24} color="white" onPress={() => setShowSearch(!showSearch)} />
                 }
                 <Entypo style={styles.settingsIcon} name="dots-three-vertical" size={24} color="white" onPress={() => setShowSettings(!showSettings)} />
             </View>
              
-            {showSearch ? <TextInput style={styles.searchInput} placeholder="Search WrestleTalk" autoFocus></TextInput> : null}
+            {showSearch ? <TextInput style={styles.searchInput} placeholder="Search WrestleTalk" autoFocus autoCapitalize="none" autoCorrect={false}></TextInput> : null}
             {
                 showSettings ? 
-                <View>
-
+                <View style={styles.settingsDropdown}>
+                    <Text style={styles.settingsItem}>Settings</Text>
+                    <Text style={styles.settingsItem}>Settings</Text>
                 </View>
                 : 
                 null
@@ -71,6 +70,17 @@ const styles = StyleSheet.create({
         paddingVertical: 5,
         fontSize: 18,
         borderRadius: 3
+    },
+    settingsDropdown: {
+        position: 'absolute',
+        top: 30,
+        right: 20,
+        backgroundColor: '#FFF',
+        borderRadius: 5
+    },
+    settingsItem: {
+        padding: 20,
+        fontSize: 18,
     }
 });
   

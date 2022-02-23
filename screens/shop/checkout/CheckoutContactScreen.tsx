e3c6131866a36eff790fc937ad8283b815ec7a49
Mo Almass <mo@horizonbirds.com>
2022-02-23 12:15:19 +0000

    Checkout > Contact UI

--- screens/shop/checkout/CheckoutContactScreen.tsx
+++ screens/shop/checkout/CheckoutContactScreen.tsx
@@ -1,11 +1,74 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import checkoutStyles from './CheckoutStyle.style';
 
-export default function CheckoutContactScreen() {
+export default function CheckoutContactScreen({ navigation }: any) {
   return (
     <ScrollView style={checkoutStyles.container}>
-      <Text style={checkoutStyles.title}>Contact</Text>
+      <View style={checkoutStyles.section}>
+        <Text style={checkoutStyles.title}>Contact Information</Text>
+        <Text style={checkoutStyles.text}>Already have an account? <Text style={checkoutStyles.textLink}>Log in</Text></Text>
+
+        <View style={[checkoutStyles.splitSection, checkoutStyles.inputSection]}>
+          <View style={checkoutStyles.splitChild}>
+            <Text style={[checkoutStyles.text, checkoutStyles.label]}>First Name</Text>
+            <TextInput style={checkoutStyles.input}></TextInput>
+          </View>
+          <View style={checkoutStyles.splitChild}>
+            <Text style={[checkoutStyles.text, checkoutStyles.label]}>Last Name</Text>
+            <TextInput style={checkoutStyles.input}></TextInput>
+          </View>
+        </View>
+
+        <View style={[checkoutStyles.inputSection]}>
+          <Text style={[checkoutStyles.text, checkoutStyles.label]}>Email</Text>
+          <TextInput style={checkoutStyles.input}></TextInput>
+        </View>
+      </View>
+
+
+      <View style={checkoutStyles.section}>
+        <Text style={checkoutStyles.title}>Shipping Address</Text>
+
+        <View style={[checkoutStyles.inputSection]}>
+          <Text style={[checkoutStyles.text, checkoutStyles.label]}>Country/Region</Text>
+          <TextInput style={checkoutStyles.input}></TextInput>
+        </View>
+
+        <View style={[checkoutStyles.inputSection]}>
+          <Text style={[checkoutStyles.text, checkoutStyles.label]}>Address</Text>
+          <TextInput style={checkoutStyles.input}></TextInput>
+        </View>
+
+        <View style={[checkoutStyles.inputSection]}>
+          <Text style={[checkoutStyles.text, checkoutStyles.label]}>Address Details</Text>
+          <TextInput style={checkoutStyles.input}></TextInput>
+        </View>
+
+        <View style={[checkoutStyles.inputSection]}>
+          <Text style={[checkoutStyles.text, checkoutStyles.label]}>City</Text>
+          <TextInput style={checkoutStyles.input}></TextInput>
+        </View>
+
+        <View style={[checkoutStyles.splitSection, checkoutStyles.inputSection]}>
+          <View style={checkoutStyles.splitChild}>
+            <Text style={[checkoutStyles.text, checkoutStyles.label]}>County</Text>
+            <TextInput style={checkoutStyles.input}></TextInput>
+          </View>
+          <View style={checkoutStyles.splitChild}>
+            <Text style={[checkoutStyles.text, checkoutStyles.label]}>Post Code</Text>
+            <TextInput style={checkoutStyles.input}></TextInput>
+          </View>
+        </View>
+      </View>
+
+      <Button onPress={() => { navigation.navigate('Checkout')}} title="GO TO PAYMENT" color="#000" /> 
+      <Text style={checkoutStyles.goBack}>Go Back</Text>
+      
+      <Text>&nbsp;</Text>
+      <Text>&nbsp;</Text>
+      <Text>&nbsp;</Text>
+
     </ScrollView>
   );
 }
--- screens/shop/checkout/CheckoutStyle.style.tsx
+++ screens/shop/checkout/CheckoutStyle.style.tsx
@@ -3,11 +3,49 @@ import { StyleSheet } from 'react-native';
 const checkoutStyles = StyleSheet.create({
   container: {
     backgroundColor: '#FFF',
-    flex: 1
+    flex: 1,
+    padding: 30
+  },
+  section: {
+    marginBottom: 30
   },
   title: {
-    fontSize: 20,
-    textAlign: 'center'
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  text: {
+    color: '#999'
+  },
+  textLink: {
+    color: 'red'
+  },
+  inputSection: {
+    marginTop: 20
+  },
+  splitSection: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  splitChild: {
+    display: 'flex',
+    width: '48%',
+  }, 
+  label: {
+    marginBottom: 5
+  },
+  input: {
+    borderColor: '#CCC',
+    borderWidth: 1,
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 5
+  },
+  goBack: {
+    textTransform: 'uppercase',
+    color: '#777',
+    textAlign: 'center',
+    padding: 20
   }
 });
 

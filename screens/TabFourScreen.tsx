84a80856a0943b4d1b6d059afbc746b7b79a1d29
Mo Almass <mo@horizonbirds.com>
2022-02-28 19:55:51 +0000

    Offers - removed dummy content

--- screens/TabFourScreen.tsx
+++ screens/TabFourScreen.tsx
@@ -1,31 +1,15 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import appStyles from '../styles/appStyles.style';
 
 export default function TabFourScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Offers</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabFourScreen.tsx" />
+    <View style={appStyles.container}>
+      <Text style={appStyles.title}>Offers</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
 });
